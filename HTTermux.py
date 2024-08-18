6a363f629c599ce55cb2c56300445ad60679fd3c
TheMaster1127 <Dzemilefere@gmail.com>
2024-08-18 14:01:11 +0300

    save

---
+++ HTTermux.py
@@ -0,0 +1,37 @@
+from flask import Flask, send_file, request, jsonify
+import os
+variables = {}
+app = Flask(__name__)
+
+@app.route('/')
+def app_route():
+    return send_file(os.path.join(os.path.dirname(__file__), 'index.html')), 200
+
+# Define a dictionary to store dynamic variables
+variables = {}
+
+import subprocess
+def RunCMD(command):
+    try:
+        result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        return e.stdout + "\n" + e.stderr
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return ""
+
+@app.route('/test', methods=['POST'])
+def test():
+    variables['command'] = request.get_json()
+    variables['data'] = RunCMD(variables['command'])
+    return variables['data']
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return "Page not found", 404
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
